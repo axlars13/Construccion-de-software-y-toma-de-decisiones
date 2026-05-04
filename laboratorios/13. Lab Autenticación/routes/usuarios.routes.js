9f6b4e76de511b3e2819c4f6b65357854018b430
axlars13 <axelorlando66@gmail.com>
2026-05-04 00:04:32 -0600

    Subo laboratorio autenticacion

---
+++ laboratorios/13. Lab Autenticación/routes/usuarios.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../controllers/usuarios.controller.js");
+const isAuth = require("../util/is-auth.js");
+
+router.get('/login', controller.render_login);
+router.post('/login', controller.do_login);
+router.get('/registro', controller.get_registro);
+router.post('/registro', controller.post_registro);
+router.get('/logout', controller.logout);
+
+router.get("/logged", isAuth, controller.get_logged);
+
+module.exports = router;
