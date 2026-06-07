ab3792d05d5961e7f48b6abdb6e9d587b1672c29
axlars13 <axelorlando66@gmail.com>
2026-06-06 20:48:30 -0600

    Subo Lab 22 Subir y bajar archivos

---
+++ laboratorios/Lab 22 Subir y bajar archivos/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const port = 4001;
+const log = console.log;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const controller = require("./index.controller");
+app.post("/upload_file", controller.upload_file);
+app.post("/upload_file_private", controller.upload_file_private);
+app.get("/get_private_file/:file", controller.get_private_file);
+
+app.listen(port, ()=>{
+    log("Node.js server running on port " + port);
+});
